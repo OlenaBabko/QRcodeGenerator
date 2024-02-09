3afe5caf5ec870c64643a5def2eb67f980b8f0a1
Olena Babko <elena.babko@gmail.com>
2024-02-09 12:26:35 +0200

    label anchor,  pad

--- my_QR.py
+++ my_QR.py
@@ -28,15 +28,15 @@ title_Label.pack(pady=50)
 
 # INPUT TEXT/URL TO GET QRcode
 getURL_Label = Label(root, text="Enter the URL(text): ", fg='black', font=('Courier', 12, 'bold'))
-getURL_Label.pack()
+getURL_Label.pack(anchor=CENTER, pady=10)
 getURL_textbox = Entry(root)
 getURL_textbox.pack()
 
 
 
 # GETTING INPUT OF SIZE OF QRcode
-qr_size_Label = Label(root, text="Size from 1 to 40 with 1 being 21x21: ", fg='black', font=('Courier', 12, 'bold'))
-qr_size_Label.pack()
+qr_size_Label = Label(root, text="Size from 1 to 40 (1=21x21): ", fg='black', font=('Courier', 12, 'bold'))
+qr_size_Label.pack(anchor=CENTER, pady=10)
 qr_size_textbox = Entry(root)
 qr_size_textbox.pack()
 
@@ -44,7 +44,7 @@ qr_size_textbox.pack()
 
 # GETTING INPUT OF LOCATION TO SAVE QRcode
 qr_location_Label = Label(root, text="Location to save the QR Code: ", font=('Courier', 12, 'bold'))
-qr_location_Label.pack()
+qr_location_Label.pack(anchor=CENTER, pady=10)
 qr_location_textbox = Entry(root)
 qr_location_textbox.pack()
 
@@ -52,7 +52,7 @@ qr_location_textbox.pack()
 
 # GETTING INPUT OF QRcode IMAGE NAME
 qr_name_Label = Label(root, text="Name of the QR Code: ", font=('Courier', 12, 'bold'))
-qr_name_Label.pack()
+qr_name_Label.pack(anchor=CENTER, pady=10)
 qr_name_textbox = Entry(root)
 qr_name_textbox.pack()
 
